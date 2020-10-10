355627afeb7c67c48e53f7ae62593a7ba9874bef
robertsaj2 <robertsaj2@gmail.com>
2020-10-09 21:10:44 -0700

    got email working again...

--- initialize_gmail_creds.py
+++ initialize_gmail_creds.py
@@ -22,8 +22,8 @@ def main():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists(os.path.join('.credentials','token.pickle')):
-        with open(os.path.join('.credentials','token.pickle'), 'rb') as token:
+    if os.path.exists(os.path.join('credentials','token.pickle')):
+        with open(os.path.join('credentials','token.pickle'), 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -31,10 +31,10 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                os.path.join('.credentials','credentials.json'), SCOPES)
+                os.path.join('credentials','credentials.json'), SCOPES)
             creds = flow.run_local_server()
         # Save the credentials for the next run
-        with open(os.path.join('.credentials','token.pickle'), 'wb') as token:
+        with open(os.path.join('credentials','token.pickle'), 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('gmail', 'v1', credentials=creds)
